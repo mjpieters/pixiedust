d7d85a877db360a7b56a0d895a16f6a74082c293
Martijn Pieters <mj@zopatista.com>
2018-09-29 17:04:28 +0100

    Include license notices in the source and readme.

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ It requires Python 3.6 or newer, run with
 $ python3 pixiedust.py program.dust
 ```
 
+## License
+
+This software may be modified and distributed under the terms of the MIT
+license. See the LICENSE.txt file for details.
--- pixiedust.py
+++ pixiedust.py
@@ -1,3 +1,8 @@
+# Copyright 2018 Martijn Pieters, Zopatista Ltd.
+#
+# This software may be modified and distributed under the terms
+# of the MIT license.  See the LICENSE.txt file for details.
+
 import operator
 import re
 import sqlite3
